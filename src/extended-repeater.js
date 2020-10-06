ba0ea3b8afbec20af9a3f2e003370bdffd056687
Projmix <balb310372@mail.ru>
2020-10-07 02:07:46 +0300

    feat: add solution tasks extended repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  this.repeatTimes =  options.repeatTimes;
+  this.separator =  String(options.separator);
+  this.addition = String(options.addition);
+  this.additionRepeatTimes = options.additionRepeatTimes;
+  this.additionSeparator =  String(options.additionSeparator);
+
+  if(options.repeatTimes === undefined)  this.repeatTimes =  1;
+  if(options.separator === undefined) this.separator = '+'; 
+  if(options.additionRepeatTimes ==undefined) this.additionRepeatTimes = 1; 
+  if(options.additionSeparator === undefined) this.additionSeparator = '+';  
+  if (options.addition === undefined)  this.addition = "";
+
+  let string='';
+
+  for (let i = 0; i < this.repeatTimes; i++) {
+    string += str;
+    for (let j = 0; j < this.additionRepeatTimes; j++) {
+      string += this.addition;
+      j < (this.additionRepeatTimes-1) ? string += this.additionSeparator : 0;
+    }
+    i < (this.repeatTimes - 1) ? string += this.separator : 0;
+  }
+
+  return string;
 };
-  
