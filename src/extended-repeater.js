09fa31d32faf568b10ae8577fbe32918e7963251
Projmix <balb310372@mail.ru>
2020-10-07 02:09:33 +0300

    feat: fix if

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,20 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function repeater(str, options) {
-  this.repeatTimes =  options.repeatTimes;
-  this.separator =  String(options.separator);
-  this.addition = String(options.addition);
-  this.additionRepeatTimes = options.additionRepeatTimes;
-  this.additionSeparator =  String(options.additionSeparator);
-
   if(options.repeatTimes === undefined)  this.repeatTimes =  1;
+  else this.repeatTimes =  options.repeatTimes;
   if(options.separator === undefined) this.separator = '+'; 
+  else this.separator =  String(options.separator);
   if(options.additionRepeatTimes ==undefined) this.additionRepeatTimes = 1; 
-  if(options.additionSeparator === undefined) this.additionSeparator = '+';  
+  else this.additionRepeatTimes = options.additionRepeatTimes;
+  if(options.additionSeparator === undefined) this.additionSeparator = '+'; 
+  else  this.additionSeparator =  String(options.additionSeparator); 
   if (options.addition === undefined)  this.addition = "";
-
+  else this.addition = String(options.addition);
   let string='';
-
   for (let i = 0; i < this.repeatTimes; i++) {
     string += str;
     for (let j = 0; j < this.additionRepeatTimes; j++) {
