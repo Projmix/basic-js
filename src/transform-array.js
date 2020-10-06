eed0b77ea8a8e28bf1c81f4bd89ac5f361f42415
Projmix <balb310372@mail.ru>
2020-10-06 17:39:51 +0300

    feat: add solution tasks transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,37 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+	if (arr == []) return [];
+	
+	const array = [];
+
+	for (let i = 0; i < arr.length; i++) {
+		switch (arr[i]) {
+			case "--discard-next":
+        if (i == arr.length - 1) continue;
+				i++;
+				break;
+
+			case "--discard-prev":
+				if (i == 0) continue;
+				if (arr[i - 2] === '--discard-next') continue;
+				array.pop();
+				break;
+
+			case "--double-next":
+				if (i == arr.length - 1) continue;
+				array.push(arr[i + 1]);
+				break;
+
+			case "--double-prev":
+        if (i == 0) continue;
+				if (arr[i - 2] === '--discard-next') continue;
+				array.push(arr[i - 1]);
+				break;
+
+			default: 
+				array.push(arr[i]);
+		}
+	}
+	return array;
 };
